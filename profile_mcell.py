0bb58505933e76dec849124be4250c52187cfc99
Jacob Czech <czech.jacob@gmail.com>
2017-03-31 17:50:24 -0400

    Clone and build mcell and nutmeg for tests.
    This requires git and cmake.

--- profile_mcell.py
+++ profile_mcell.py
@@ -4,10 +4,11 @@ import time
 import os
 import yaml
 import argparse
+import shutil
 
 
-def get_mcell_vers():
-    proc = subprocess.Popen(['mcell'], stdout=subprocess.PIPE)
+def get_mcell_vers(mcell_bin):
+    proc = subprocess.Popen([mcell_bin], stdout=subprocess.PIPE)
     output = proc.stdout.readlines()
     first_line = output[0].decode('UTF-8').split()
     vers = first_line[1]
@@ -32,9 +33,9 @@ def parse_test():
     return (mdl_name, categories, command_line_opts)
 
 
-def run_mcell(mdl_name, command_line_opts):
+def run_mcell(mcell_bin, mdl_name, command_line_opts):
     seed = random.randint(1, 2147483647)
-    command = ['mcell', '-seed', '%d' % seed, mdl_name]
+    command = [mcell_bin, '-seed', '%d' % seed, mdl_name]
     command.extend(command_line_opts)
     start = time.time()
     subprocess.call(
@@ -42,6 +43,31 @@ def run_mcell(mdl_name, command_line_opts):
     end = time.time()
     elapsed_time = end-start
     return elapsed_time
+
+
+def build_nutmeg():
+    subprocess.call(['git', 'clone', 'https://github.com/mcellteam/nutmeg'])
+    os.chdir("nutmeg")
+    subprocess.call(['git', 'pull'])
+    os.chdir("tests")
+    os.chdir("../..")
+
+
+def build_mcell():
+    subprocess.call(['git', 'clone', 'https://github.com/mcellteam/mcell'])
+    os.chdir("mcell")
+    subprocess.call(['git', 'pull'])
+    build_dir = "build"
+    shutil.rmtree(build_dir, ignore_errors=True)
+    os.mkdir(build_dir)
+    os.chdir(build_dir)
+    subprocess.call(["cmake", ".."])
+    subprocess.call(["make"])
+    mcell_dir = os.getcwd()
+    mcell_bin = os.path.join(mcell_dir, "mcell")
+    os.chdir("../..")
+
+    return mcell_bin
 
 
 def setup_argparser():
@@ -54,10 +80,15 @@ def setup_argparser():
 def main():
     args = setup_argparser()
     category = args.category
+
+    build_nutmeg()
+    mcell_bin = build_mcell()
+
     mdl_times = {}
+    os.chdir("nutmeg/tests")
     dirs = os.listdir(os.getcwd())
     dirs.sort()
-    mcell_vers, commit = get_mcell_vers()
+    mcell_vers, commit = get_mcell_vers(mcell_bin)
     all_stuff = {'mcell_vers': mcell_vers, 'commit': commit}
     for dirn in dirs:
         if not os.path.isdir(dirn):
@@ -66,7 +97,7 @@ def main():
         mdl_name, categories, command_line_opts = parse_test()
         mdl_dir_fname = "{0}/{1}".format(dirn, mdl_name)
         if category in categories:
-            elapsed_time = run_mcell(mdl_name, command_line_opts)
+            elapsed_time = run_mcell(mcell_bin, mdl_name, command_line_opts)
             mdl_times[mdl_dir_fname] = elapsed_time
         os.chdir("..")
     total_time = sum([mdl_times[k] for k in mdl_times])
